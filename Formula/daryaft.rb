7f1fd3df89407e57c85447b6146c4e4c7d10ce86
AmirHesam Piri <amirhesam.piri@digikala.com>
2026-06-10 17:33:51 +0330

    Update daryaft to v1.8.0

--- Formula/daryaft.rb
+++ Formula/daryaft.rb
@@ -1,15 +1,15 @@
 class Daryaft < Formula
   desc "Modern terminal downloader with CLI and TUI"
   homepage "https://github.com/he8um/daryaft"
-  version "1.7.0"
+  version "1.8.0"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/he8um/daryaft/releases/download/v1.7.0/daryaft_darwin_arm64.tar.gz"
-    sha256 "5de062f8f36b889e88d7eb05a65c5e8af6b8768f9299ef94624baf8facbf2960"
+    url "https://github.com/he8um/daryaft/releases/download/v1.8.0/daryaft_darwin_arm64.tar.gz"
+    sha256 "350e0278f749d195a3fae6a21d04ac06bf3c4b064e4568a3973d8a08905cc406"
   else
-    url "https://github.com/he8um/daryaft/releases/download/v1.7.0/daryaft_darwin_amd64.tar.gz"
-    sha256 "987a245768ba5e675ff6e0d2d9f42690f70c2d300e723b757522936422616ab1"
+    url "https://github.com/he8um/daryaft/releases/download/v1.8.0/daryaft_darwin_amd64.tar.gz"
+    sha256 "c02710f09cb2878645e1a40aa281876b46229a23893eb724c9cd4eec779e8968"
   end
 
   def install
